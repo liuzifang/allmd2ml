799d035885d2a03cbb5893bcce57ed3262f28459
子方 <l.z.fang@sohu.com>
2014-03-10 10:55:18 +0800

    1.1.0

--- src/allmd2ml/core.clj
+++ src/allmd2ml/core.clj
@@ -28,8 +28,10 @@
       readbuf)
     @content))
 
+;删除过程中多获得了一个无意义的对象，这是三个冗余设计中的其中一个。不过我认为要是节约这点资源的话，就压根不应该使用这个程序。它本来就是在积压太多md时使用的。
 (defn gen-ml [mdlist mlist]
   (let [md-content (read-str (first mdlist))]
+    (let [file (File. (first mlist))] (when (.exists file) (.delete file)))
     (zf/writefile (first mlist) (md/md-to-html-string md-content)))
   (when-not (empty? (rest mlist)) (recur (rest mdlist) (rest mlist))))
 
